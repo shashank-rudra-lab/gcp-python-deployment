177fe99149740e0472732b68ee6aa87386f8a15b
sairudra <sairudra@geotab.com>
2025-03-25 14:58:21 -0400

    test

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="my-python-app",
     version="0.1",
-    packages=["."],  # Include current directory
+    py_modules=["main"],  # Directly reference main.py
     install_requires=["Flask"],
 )
